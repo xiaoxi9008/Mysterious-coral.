93caf12e9d4f23b27d86e2377cfa486b49a554a4
xuaoxi9008 <3574769415@qq.com>
2026-04-18 07:24:15 +0800

    Add files via upload

---
+++ 防止俄亥俄XIAOXI.lua
@@ -0,0 +1,13 @@
+/game.StarterGui:SetCore("SendNotification", {
+    Title = "欢迎使用XIAOXI HUB",
+    Text = "正在为你打开加载器…",
+    Duration = 3 
+})
+task.wait(3) 
+if game.PlaceId == 7239319209 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E4%BF%84%E4%BA%A5%E4%BF%84%E5%B7%9E%E6%BA%90%E7%A0%81.lua"))()
+ else
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Mysterious-coral./refs/heads/main/XIAOXI付费加载器.lu/a"))()
+
+end
