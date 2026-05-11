501a4354b18aaf17be73bcb9531dde00072494cd
xuaoxi9008 <3574769415@qq.com>
2026-05-11 19:44:21 +0800

    Add files via upload

--- XIAOXI付费版服务器id.lua
+++ XIAOXI付费版服务器id.lua
@@ -1,70 +1,125 @@
 game.StarterGui:SetCore("SendNotification", {
-    Title = "XIAOXI检测中",
+    Title = "XIAOXI HUB检测中",
     Text = "正在为你自动检测服务器...",
-    Duration = 3 
+    Duration = 3
 })
-task.wait(3) 
-if game.PlaceId == 94101948530988 then 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E8%B5%9B%E9%A9%AC%E5%A8%98.lua"))()
 
- elseif game.PlaceId == 7239319209 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E4%BF%84%E4%BA%A5%E4%BF%84%E5%B7%9E%E6%BA%90%E7%A0%81.lua"))()
-     
-elseif game.PlaceId == 286090429 then 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E5%B0%8F%E8%A5%BF%E5%85%B5%E5%B7%A5%E5%8E%82.lua"))()
+task.wait(3)
 
-elseif game.PlaceId == 79393329652220 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E5%B0%8F%E8%A5%BF%E9%98%B2%E5%BE%A1.lua"))() 
-  
-elseif game.PlaceId == 4639625707 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E6%88%98%E4%BA%89%E5%A4%A7%E4%BA%A8.lua"))()
-  
-elseif game.PlaceId == 126509999114328 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/99夜.lua"))() 
-     
-elseif game.PlaceId == 100484168444874 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/决斗场.lua"))() 
-     
-elseif game.PlaceId == 94217045453265 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/决斗场.lua"))() 
-     
-elseif game.PlaceId == 6516141723 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))() 
+if game.PlaceId == 94101948530988 then -- 赛马娘
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E8%B5%9B%E9%A9%AC%E5%A8%98.lua"))()
 
-elseif game.PlaceId == 6839171747 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))() 
+elseif game.PlaceId == 7239319209 then -- 俄亥俄州
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Mysterious-coral./refs/heads/main/%E4%BF%84%E4%BA%A5%E4%BF%84%E9%80%9A%E7%9F%A5.lua"))()
 
-elseif game.PlaceId == 10449761463 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/最强战场.lua"))() 
+elseif game.PlaceId == 286090429 then -- 兵工厂
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E5%B0%8F%E8%A5%BF%E5%85%B5%E5%B7%A5%E5%8E%82.lua"))()
 
-elseif game.PlaceId == 83645629621104 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXIHUB被遗弃.lua"))() 
-     
-elseif game.PlaceId == 18687417158 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXIHUB被遗弃.lua"))() 
+elseif game.PlaceId == 79393329652220 then -- 小西防御
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E5%B0%8F%E8%A5%BF%E9%98%B2%E5%BE%A1.lua"))()
 
-elseif game.PlaceId == 11815767793 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/终极战场.lua"))() 
+elseif game.PlaceId == 4639625707 then -- 战争大亨
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E6%88%98%E4%BA%89%E5%A4%A7%E4%BA%A8.lua"))()
 
-elseif game.PlaceId == 101993432229107 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/终极战场.lua"))() 
-     
- elseif game.PlaceId == 70632463582033 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/终极战场.lua"))()     
- 
-elseif game.PlaceId == 87018676608089 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXI手枪竞技场.lua"))()     
+elseif game.PlaceId == 126509999114328 then -- 99夜
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/99夜.lua"))()
 
-elseif game.PlaceId == 85050171250159 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/拉大便.lua"))()     
+elseif game.PlaceId == 100484168444874 then -- 决斗场
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/决斗场.lua"))()
 
-elseif game.PlaceId == 81431295813006 then
-     loadstring(game:HttpGet("http://121.43.37.20:8885/output/enc/084b2e5e3026"))() 
-                       
- elseif game.PlaceId == 136801880565837 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E9%97%AA%E5%85%89.lua"))()
- else
+elseif game.PlaceId == 94217045453265 then -- 决斗场(第二地图)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/决斗场.lua"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/通用.lua"))()
+-- DOORS 全地图
+elseif game.PlaceId == 6516141723 then -- DOORS 主游戏
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
 
+elseif game.PlaceId == 6839171747 then -- DOORS Lobby
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
+
+elseif game.PlaceId == 10549820578 then -- The Backdoor
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
+
+elseif game.PlaceId == 10873387885 then -- The Rooms
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
+
+elseif game.PlaceId == 12210359224 then -- Floor 2 The Mines
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
+
+elseif game.PlaceId == 123542004387962 then -- Super Hard Mode
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
+
+elseif game.PlaceId == 12696293714 then -- Retro Mode
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
+
+elseif game.PlaceId == 13728192813 then -- April Fools Mode
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/DOORS加载器。.lua"))()
+
+elseif game.PlaceId == 10449761463 then -- 最强战场
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/最强战场.lua"))()
+
+elseif game.PlaceId == 83645629621104 then -- 被遗弃
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXIHUB被遗弃.lua"))()
+
+elseif game.PlaceId == 18687417158 then -- 被遗弃(第二地图)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXIHUB被遗弃.lua"))()
+
+elseif game.PlaceId == 11815767793 then -- 终极战场
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/终极战场通知.lua"))()
+
+elseif game.PlaceId == 101993432229107 then -- 终极战场(第二地图)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/终极战场.lua"))()
+
+elseif game.PlaceId == 87018676608089 then -- 手枪竞技场
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXI手枪竞技场.lua"))()
+
+elseif game.PlaceId == 85050171250159 then -- 拉大便
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/拉大便.lua"))()
+
+elseif game.PlaceId == 81431295813006 then --卡塔纳竞技场
+    loadstring(game:HttpGet("http://121.43.37.20:8885/output/enc/084b2e5e3026"))()
+
+elseif game.PlaceId == 136801880565837 then -- 闪光
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E9%97%AA%E5%85%89.lua"))()
+
+elseif game.PlaceId == 4588604953 then -- Criminality 主地图
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/%E7%8A%AF%E7%BD%AA%E9%80%9A%E7%9F%A5.lua"))()
+
+elseif game.PlaceId == 8343259840 then -- Criminality Casual
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/%E7%8A%AF%E7%BD%AA%E9%80%9A%E7%9F%A5.lua"))()
+
+elseif game.PlaceId == 15169303036 then -- Criminality Standard
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/%E7%8A%AF%E7%BD%AA%E9%80%9A%E7%9F%A5.lua"))()
+
+elseif game.PlaceId == 15169351413 then -- Criminality C-Standard
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/%E7%8A%AF%E7%BD%AA%E9%80%9A%E7%9F%A5.lua"))()
+
+elseif game.PlaceId == 15169316384 then -- Criminality M-Casual
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/%E7%8A%AF%E7%BD%AA%E9%80%9A%E7%9F%A5.lua"))()
+
+elseif game.PlaceId == 15169310267 then -- Criminality Infection
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/%E7%8A%AF%E7%BD%AA%E9%80%9A%E7%9F%A5.lua"))()
+
+elseif game.PlaceId == 15496696918 then -- Criminality Banland
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/%E7%8A%AF%E7%BD%AA%E9%80%9A%E7%9F%A5.lua"))()
+    
+elseif game.PlaceId == 12334109280 then -- Guts & Blackpowder
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/GB通知2.lua"))()
+
+elseif game.PlaceId == 14218103727 then -- G&B VC Server
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/GB通知2.lua"))() 
+    
+-- PETAPETA / 无限旅馆
+
+elseif game.PlaceId == 14476003462 then -- PETAPETA / 无限旅馆 第1章
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXI无限旅馆（阉割版）.lua"))()
+
+elseif game.PlaceId == 79036558298014 then -- PETAPETA 梦魇学校 / 第2章
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/XIAOXI无限旅馆第2章付费版.lua"))()
+
+elseif game.PlaceId == 15085229984 then -- PETAPETA WIP测试服
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/XIAOXI无限旅馆（阉割版）.lua"))()          
+
+else -- 不支持服务器
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Mysterious-coral./refs/heads/main/%E4%B8%8D%E6%94%AF%E6%8C%81%E6%9C%8D%E5%8A%A1%E5%99%A8%E9%80%9A%E7%9F%A5.lua"))()
 end
