178708367385bb5abfe4fbd55d35fcc3002a09e5
xuaoxi9008 <3574769415@qq.com>
2026-06-03 16:35:29 +0800

    Add files via upload

--- 防止俄亥俄XIAOXI.lua
+++ 防止俄亥俄XIAOXI.lua
@@ -10,7 +10,7 @@ print("✅ XIAOXI HUB - 正在加载...")
 task.wait(3) 
 
 if game.PlaceId == 7239319209 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E4%BF%84%E4%BA%A5%E4%BF%84%E5%B7%9E%E6%BA%90%E7%A0%81.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Mysterious-coral./refs/heads/main/XIAOXI%E4%BF%84%E4%BA%A5%E4%BF%84%E5%B7%9EV5.lua"))()
     
 elseif game.PlaceId == 117398147513099 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/XIAOXI付费版竞争对手.lua"))()
